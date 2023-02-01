7ffc98438927e14259e1becf4b01a97e018e983f
souvik666 <peacedynasty0@gmail.com>
2023-02-01 13:34:48 +0530

    feat(deploy)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,7 @@
-import { Button, ContactUs } from "@components";
+import { Button } from "@components";
 import { NavigateFunction, useNavigate } from "react-router-dom";
+
+import logo from "../../assets/react.svg";
 
 export default function Home() {
   const navigate: NavigateFunction = useNavigate();
@@ -8,8 +10,8 @@ export default function Home() {
     navigate("/products");
   }
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: 12 }}>
-      <ContactUs />
+    <div style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+      <img alt="react-logo" src={logo} />
       <Button label="show Products" onclick={HandleNav} />
     </div>
   );
